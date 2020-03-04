43e76aa0d6a80bcba6fbdb4bbc44b2a370f7915c
SRodriguess <s.rodriguescn@gmail.com>
2020-03-04 16:26:11 -0300

    Myers Otimized

--- source/algos/myers_otimized.cpp
+++ source/algos/myers_otimized.cpp
@@ -25,18 +25,20 @@
 
 using namespace std;
 
-void MReport(int j, unsigned long OM){
+void MReport(int j, unsigned long long OM){
 
-			while (OM != 0x0000000000000000){
-				
-				int s = floor(log2(OM));
-				cerr << "s = " << s << endl;
-				cerr << "MATCH = " << j << endl;
-				cerr << "OM = " << OM << endl;
-				OM = OM & (~(1 << s));
-				cerr << "-----------------" << endl;
+	while (OM != 0){
+		
+		int s = floor(log2(OM));
+		cerr << "\n-----------------" << endl;
+		// cerr << "s = " << s << endl;
+		cerr << "MATCH = " << j << endl;
+		cerr << "OM1 = " << OM << endl;
+		OM = OM & ~(1 << s);
+		cerr << "OM2 = " << OM << endl;
+		cerr << "-----------------\n" << endl;
 
-			}
+	}
 
 }
 
@@ -48,12 +50,14 @@ vector<int> search(char *PP, long m, char *T, long n){
 
 	// cerr << "SIZE T > " << strlen(T) << endl;
 
-	strcpy(conc, "ATATATATATATATATATATATATATATATAT");
+	// strcpy(conc, "ATATATATATATATATATATATATATATATAT");
+	strcpy(conc, "CAGGTCGTAACTGTGGTCAGCCGAGTTAAGCG");
 
 	strcpy(P, conc);
 	strcat(P, PP);
 
 	cerr << P << endl;
+	cerr << T << endl;
 
 	unsigned long k = 0;
 	
@@ -62,45 +66,46 @@ vector<int> search(char *PP, long m, char *T, long n){
 	int MAXCHAR = 256;
 
 	// MComputePM //////////////////////////////////////// x
-	unsigned long PM[MAXCHAR];
-	for (int x = 0; x < MAXCHAR; x++) PM[x] = 0;
+	unsigned long long PM[MAXCHAR];
+	for (int x = 0; x < MAXCHAR; x++) PM[x] = 0x0000000000000000;
 
 	int w = 64;
-	// int r = w / m;
-	int r = 2;
-	unsigned long constt, constt2, tmp;
+	int r = w / m;
+	// int r = 2;
+	unsigned long long constt, constt2, tmp;
 	for (int s = 1; s <= r; s++){
 		for (int i = 1; i <= m; i++){
 			// constt2 = m*(r-s+1)-1;
 			constt = 1 << (m*(s-1)+i-1);	
-			cerr << "constt: " << constt << endl;
+			// cerr << "constt: " << constt << endl;
 			// tmp = (0 << constt2) | constt;
 			// tmp = constt;
 			// cerr << "tmp > " << tmp << endl;
-			PM[P[i-1]] = PM[P[i-1]] | constt;
+			PM[P[i-1]] |= constt;
 		}
 	}
 
 	////////////////////////////////////////////////////
 
-	unsigned long ZM = 0x7FFFFFFF7FFFFFFF; //ok
-	unsigned long EM = 0x8000000080000000; //ok
-	unsigned long VN = 0; //ok
-	unsigned long VP = 0xFFFFFFFFFFFFFFFF; //ok
+	unsigned long long ZM = 0x7FFFFFFF7FFFFFFF; //ok
+	// unsigned long long EM = 0x8000000080000000; //ok
+	unsigned long long EM = 0x80000000; //ok
+	unsigned long long VN = 0x0000000000000000; //ok
+	unsigned long long VP = 0xFFFFFFFFFFFFFFFF; //ok
 
-	unsigned long temp = pow(2, m-1);
+	unsigned long long temp = pow(2, m-1);
 	unsigned long long MC = (temp + k) * (0x0000000100000001);
 	// unsigned long long MC = (temp + k) * 4294967297;
 //	MC = (2147483648 + k) * 4294967297;	 	
 
 
-	unsigned long D0 = 0, HP = 0, HN = 0, XP = 0, XN = 0;
+	unsigned long long D0 = 0, HP = 0, HN = 0, XP = 0, XN = 0;
 
 	for (int j = 1; j <= n; j++){
 
-		cerr << "Carac: " << T[j-1] << endl;
-		cerr << "MC 1: " << MC << endl;
-		// MStep ////////////////// OK //////////////////////////////// OK
+		// cerr << "Carac: " << T[j-1] << endl;
+		// cerr << "MC 1: " << MC << endl;
+		// MStep ////////////////// OK /////////////////////////// OK
 		XP = VP & ZM;
 		D0 = (((PM[T[j-1]] & XP) + XP) ^ XP) | PM[T[j-1]] | VN;
 		HP = VN | ~(D0 | VP);
@@ -112,19 +117,18 @@ vector<int> search(char *PP, long m, char *T, long n){
 		VN = XP & D0;
 		//////////////////////////////////////////////////////////
 
-		MC = MC + ((HN & EM) >> (m-1)) - ((HP & EM) >> (m-1));
+		MC = MC + ((HN & EM) >> (m-1)) - (HP & EM) >> (m-1);
 		cerr << "MC 2: " << MC << endl;
 		
-		unsigned long tp = MC & EM;
+		unsigned long long tp = MC & EM;
 		
 		if (tp != 0x0000000000000000){
 			MReport(j, tp);		
-
 		}
 
 	}
 
-	cerr << "========================================================================================" << endl;
+	cerr << "=================================================" << endl;
 
 	return indexx;
 
